fdc34013c373c0a568855d9c03aa712120b766ed
FelipePy <felipespsampaiomotasp@gmail.com>
2022-05-24 14:48:56 -0300

    Tudo ou nada

--- organize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git pull
-mv nomades.service.example /etc/systemd/system/nomades.service
-venv/bin/pip install -r requirements.txt
-docker-compose down
-docker-compose up
-systemctl daemon-restart nomades
-/home/ubuntu/Nomades-VH/venv/bin/python /home/ubuntu/Nomades-VH/main.py
