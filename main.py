9b06e0f263f403b7c6bd2b5d4f5ca2c6ac3f21a8
FelipePy <felipespsampaiomotasp@gmail.com>
2023-12-19 22:12:39 -0300

    Refactor main

--- main.py
+++ main.py
@@ -2,40 +2,45 @@ from app.auth.hasher import hash_password
 from ports.uow import AbstractUow
 from dotenv import load_dotenv
 from os import environ
+from app.uow import SqlAlchemyUow
 
-if __name__ == "__main__":
 
+def _load_env():
     load_dotenv(".env")
 
-    # TODO: Algumas urls ainda permitem acesso sem o token de acesso
 
+def _create_tables():
     from bootstrap.database import ensure_all_entities
 
     ensure_all_entities()
 
-    from app.uow import SqlAlchemyUow
+
+def _create_root(uow: AbstractUow):
+    from app.user.entities import User
+    from general_enum.permissions import Permissions
+    from app.user.services import get_user_by_email
+
+    root_user = get_user_by_email(uow, environ.get("ROOT_USER_EMAIL"))
+
+    if not root_user:
+        with uow:
+            uow.user.add(User(
+                username=environ.get("ROOT_USER"),
+                email=environ.get("ROOT_USER_EMAIL"),
+                password=hash_password(environ.get("ROOT_USER_PASSWORD")),
+                permission=Permissions.root.value,
+                fk_band=None
+            ))
 
 
-    def _create_root(uow: AbstractUow):
-        from app.user.entities import User
-        from general_enum.permissions import Permissions
-        from app.user.services import get_user_by_email
+if __name__ == "__main__":
+    # TODO: Algumas urls ainda permitem acesso sem o token de acesso
 
-        root_user = get_user_by_email(uow, environ.get("ROOT_USER_EMAIL"))
-
-        if not root_user:
-            with uow:
-                uow.user.add(User(
-                    username=environ.get("ROOT_USER"),
-                    email=environ.get("ROOT_USER_EMAIL"),
-                    password=hash_password(environ.get("ROOT_USER_PASSWORD")),
-                    permission=Permissions.root.value,
-                    fk_band=None
-                ))
+    _load_env()
+    _create_tables()
 
     uow = SqlAlchemyUow()
-
     _create_root(uow)
-    import uvicorn
 
+    import uvicorn
     uvicorn.run(app="bootstrap.server:app", host="0.0.0.0", port=8000)
