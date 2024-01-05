844a289e05d6818f7e6d956f7f0caf3415690e90
FelipePy <felipespsampaiomotasp@gmail.com>
2024-01-04 21:51:28 -0300

    Create general update controller

--- app/poomsae/controllers.py
+++ app/poomsae/controllers.py
@@ -1,14 +1,17 @@
+from uuid import UUID
+
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi import Response
 
 from app.auth.services import get_current_user_with_permission
 from app.poomsae import services as poomsae_sv
-from app.poomsae.entities import Poomsae
+from app.poomsae.entities import Poomsae as PoomsaeEntity
 from app.poomsae.models import Poomsae
 from app.uow import SqlAlchemyUow
 from app.user.entities import User
 from app.utils.create_controller import create_controller
 from app.utils.get_controller import get_controller
+from app.utils.update_controller import update_controller
 from general_enum.permissions import Permissions
 from ports.uow import AbstractUow
 
@@ -36,9 +39,9 @@ async def get_poomsae():
 @router.post("/")
 @create_controller(poomsae_sv)
 async def create_poomsae(
-        entity: Poomsae,
+        model: Poomsae,
         message_success: str = "Poomsae criado com sucesso.",
-        message_error: str = "Não foi possível criar um Poomsae.",
+        message_error: str = "Não foi possível criar o Poomsae.",
         uow: AbstractUow = Depends(SqlAlchemyUow),
         current_user: User = Depends(get_current_user_with_permission(Permissions.table))
 ) -> Response:
@@ -46,8 +49,16 @@ async def create_poomsae(
 
 
 # TODO: Create Put Method
-@router.put("/")
-async def update_poomsae():
+@router.put("/{id}")
+@update_controller(poomsae_sv)
+async def update_poomsae(
+        id: UUID,
+        model: Poomsae,
+        message_success: str = "Poomsae atualizado com sucesso.",
+        message_error: str = "Não foi possível atualizar o Poomsae.",
+        uow: AbstractUow = Depends(SqlAlchemyUow),
+        current_user: User = Depends(get_current_user_with_permission(Permissions.table))
+):
     pass
 
 
