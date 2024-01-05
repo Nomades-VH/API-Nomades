de5b0a1a5f01b0cc038fbbfd376f02ec6099a3cc
FelipePy <felipespsampaiomotasp@gmail.com>
2024-01-05 14:25:15 -0300

    Using general controller to delete poomsae

--- app/poomsae/services.py
+++ app/poomsae/services.py
@@ -6,13 +6,12 @@ from app.poomsae.models import Poomsae as PoomsaeModel
 from ports.uow import AbstractUow
 
 
-# TODO: Create a service get_all poomsae
 def get(uow: AbstractUow) -> Iterator[PoomsaeEntity]:
     with uow:
         yield from uow.poomsae.iter()
 
 
-def add(uow: AbstractUow, poomsae):
+def add(uow: AbstractUow, poomsae: PoomsaeEntity):
     with uow:
         uow.poomsae.add(poomsae)
 
@@ -26,25 +25,15 @@ def get_by_id(uow: AbstractUow, id: UUID) -> PoomsaeEntity:
     with uow:
         return uow.poomsae.get(id)
 
-# TODO: Create a service get poomsae
-def get_poomsae():
-    ...
 
-
-# TODO: Create a service post poomsae
-def post_poomsae():
-    pass
-
-
-# TODO: Create a service put poomsae
 def update(uow: AbstractUow, poomsae: PoomsaeEntity):
     with uow:
         uow.poomsae.update(poomsae)
 
 
-# TODO: Create a service delete poomsae
-def delete_poomsae():
-    pass
+def delete(uow: AbstractUow, uuid: UUID) -> None:
+    with uow:
+        uow.poomsae.remove(uuid)
 
 
 def to_entity(entity: PoomsaeEntity, model: PoomsaeModel):
