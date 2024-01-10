e0122cb28159e68dd8a51cef0ede6f315c14d543
FelipePy <felipespsampaiomotasp@gmail.com>
2024-01-09 22:53:00 -0300

    Logs to decorators

--- app/utils/get_by_controller.py
+++ app/utils/get_by_controller.py
@@ -2,15 +2,12 @@ from functools import wraps
 from http import HTTPStatus
 from typing import TypeVar, Any
 
-from fastapi import Depends
-from loguru import logger
 from starlette.responses import Response
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
-from app.auth.services import get_current_user_with_permission
 from app.user.entities import User
-from general_enum.permissions import Permissions
+from app.utils.Logs import Logs as Log
 from ports.uow import AbstractUow
 
 T = TypeVar("T")
@@ -19,6 +16,7 @@ T = TypeVar("T")
 def get_by_controller(get_service):
     def inner(func):
         @wraps(func)
+        @Log.decorators_log(func.__module__, func.__name__)
         async def wrapper(
                 param: Any,
                 message_success: str,
@@ -26,18 +24,18 @@ def get_by_controller(get_service):
                 uow: AbstractUow,
                 current_user: User
         ) -> Response:
-            print(current_user)
-            response = await func(param, message_success, message_error, uow, current_user)
+            response: JSONResponse = await func(param, message_success, message_error, uow, current_user)
             if response:
                 return response
 
             entity = jsonable_encoder(get_service(uow, param))
 
             if not entity:
-                return JSONResponse(
+                response = JSONResponse(
                     status_code=HTTPStatus.BAD_REQUEST,
-                    content={"message": message_error + f' Identificador {param} não encontrado.'}
+                    content={"message": message_error + f' Identificador {param} não encontrado.'},
                 )
+                return response
 
             return JSONResponse(
                 status_code=HTTPStatus.OK,
@@ -45,5 +43,4 @@ def get_by_controller(get_service):
             )
 
         return wrapper
-
     return inner
