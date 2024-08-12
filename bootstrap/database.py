6ee7a22b436efee7557b5d61b48f7b6d45e9fa9f
FelipePy <felipespsampaiomotasp@gmail.com>
2024-08-12 15:56:37 -0300

    alembic configured

--- bootstrap/database.py
+++ bootstrap/database.py
@@ -6,11 +6,18 @@ from sqlalchemy.orm import registry, sessionmaker
 
 mapper_registry = registry()
 
+_URI = None
 
 if "pytest" in sys.modules:
-    _URI = os.environ['DATABASE_URI_TEST']
-else:
+    _URI = os.environ.get('DATABASE_URI_TEST')
+elif "alembic" in sys.modules:
+    _URI = os.environ.get('DATABASE_URI_ALEMBIC')
+
+if not _URI:
     _URI = os.environ.get('DATABASE_URI')
+
+if not _URI:
+    raise RuntimeError("Tente utilizar: export '$(cat .env | xargs)' ou atualizar o seu arquivo .env")
 
 
 _engine = create_engine(_URI)
@@ -40,4 +47,4 @@ def ensure_all_entities():
     # noinspection PyUnresolvedReferences
     import app.auth.orm
 
-    mapper_registry.metadata.create_all(bind=_engine)
+    mapper_registry.metadata.reflect(bind=_engine)
